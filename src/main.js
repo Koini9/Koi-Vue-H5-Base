d14fee825d5d4b7b69b8bc40a3fbb8f8f079e296
todouni <todouni@163.com>
2021-07-02 15:26:39 +0800

    router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 import "./assets/style/icon.css"
 import "./assets/style/reset.css"
@@ -19,5 +20,6 @@ Vue.prototype.$moment = moment
 Vue.config.productionTip = false
 
 new Vue({
-    render: h => h(App),
+  router,
+  render: h => h(App),
 }).$mount('#app')
